b3468274b73861ab4633dd6e04226834966adc3d
Janse309 <jansimonboecker40@gmail.com>
2026-08-27 17:29:10 +0200

    feat: add logic for login-page
    -add login for user, for testing reasons with a testUser
    -add forwarding to summary.html by succesful login

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,19 @@
+let testUser = JSON.parse(localStorage.getItem('testUser')) || [
+    { 'email': 'meinetestemail@gmail.com', 'password': 'test123' },
+];
+
 let signUp = document.getElementById('sign-up-btn');
-signUp.addEventListener("click", () =>{
+signUp.addEventListener("click", () => {
     window.location.href = "../pages/register.html"
-})
+});
+
+function userLogin(event) {
+    event.preventDefault();
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = testUser.find(user => user.email == email.value && user.password == password.value);
+    if (user) {
+        console.log("user gefunden");
+        window.location.href = './pages/summary.html';
+    }
+}
