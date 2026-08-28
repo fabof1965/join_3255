001f54db73b7e71b5669c48ff5429d151eb3c2cd
Janse309 <jansimonboecker40@gmail.com>
2026-08-28 06:09:36 +0200

    Remove comments regarding DOM manipulation in the `postData` and `patchData` functions

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -21,7 +21,6 @@ async function postData(path = "", data = {}) {
         return responseToJson = await response.json();
     } catch (error) {
         console.error('Fehler beim Hochladen der Post-Daten', error);
-        // hier eine DOM Manipulation erstellen
     }
 }
 
@@ -35,6 +34,5 @@ async function patchData(path = "", data = {}) {
         return responseToJson = await response.json();
     } catch (error) {
         console.error('Fehler beim Hochladen der Patchdaten', error);
-        // hier eine DOM Manipulation erstellen
     }
 }
