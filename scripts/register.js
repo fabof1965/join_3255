c72760cdaf9dc3030723af53a3edd5811e7819f4
Janse309 <jansimonboecker40@gmail.com>
2026-08-29 13:57:07 +0200

    feat: add user data handling functions and comments in register.js

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,29 @@
+let allUsers = [];
+
 let backToLogin = document.getElementById('arrow-back');
 backToLogin.addEventListener("click", () => {
   window.location.href = "../index.html"
 });
+
+// function getUserData() {
+//   let name = document.getElementById('name').value;
+//   let email = document.getElementById('email').value;
+//   let password = document.getElementById('password').value;
+//   let confirmPassword = document.getElementById('confirm-password').value;
+//   let userData = createUserObject(name, email, password);
+//   return userData;
+// }
+
+// function createUserObject(name, email, password) {
+//   let userObject = {
+//     "name": name,
+//     "email":email,
+//     "password": password
+//   };
+//   return userObject;
+// }
+
+// user Objekt erstellen
+
+// wie würde ich es machen 
+  // ich würde die Daten in das Array pushen und dann in der Firebase speichern
