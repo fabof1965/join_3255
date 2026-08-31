38f958068d5c54b635e213f7d7d48dd2a44aea59
fabof1965 <fabian.gerdes1996@googlemail.com>
2026-08-31 16:51:42 +0200

    add: added basic contacts page without js rendering

---
+++ scripts/contacts.js
@@ -0,0 +1,17 @@
+const indexLetter = 65;
+
+function loopThroughAlphabet() {
+    const contactContainer = document.getElementById("contacts");
+
+    for (let i = 65; i <= 90; i++) {
+        // const letter = String.fromCharCode(i);
+        // const div = document.createElement("div");
+        // div.innerHTML = letter;
+        // contactContainer.innerHTML += div;
+        console.log(String.fromCharCode(i));
+    }
+}
+
+function makeElementFromLetter(letter) {
+
+}
