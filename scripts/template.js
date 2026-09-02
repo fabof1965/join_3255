b147673d7a6f0d6fe68f996b3c37d12d201562fb
fabof1965 <fabian.gerdes1996@googlemail.com>
2026-09-02 08:30:29 +0200

    refactor: semantic correct sidebar changes

--- scripts/template.js
+++ scripts/template.js
@@ -1,28 +1,26 @@
 const sidebarTemplate = `
-  <aside class="sidebar">
-      <nav class="sidebar-navigation">
-        <a href="./summary.html">
-          <img src="../assets/icons/summary.svg" alt="" />
-          <span>Summary</span>
-        </a>
-        <a href="">
-          <img src="../assets/icons/add_task.svg" alt="" />
-          <span>Add Task</span>
-        </a>
-        <a href="../pages/board.html">
-          <img src="../assets/icons/board.svg" alt="" />
-          <span>Board</span>
-        </a>
-        <a href="">
-          <img src="../assets/icons/contacts.svg" alt="" />
-          <span>Contacts</span>
-        </a>
-      </nav>
-      <div class="sidebar-footer">
-        <a class="privacy-policy-link" href="./privacy_policy.html">Privacy Policy</a>
-        <a class="legal-notice-link" href="./legal_notice.html">Legal Notice</a>
-      </div>
-    </aside>
+    <nav class="sidebar-navigation">
+      <a href="./summary.html">
+        <img src="../assets/icons/summary.svg" alt="" />
+        <span>Summary</span>
+      </a>
+      <a href="">
+        <img src="../assets/icons/add_task.svg" alt="" />
+        <span>Add Task</span>
+      </a>
+      <a href="../pages/board.html">
+        <img src="../assets/icons/board.svg" alt="" />
+        <span>Board</span>
+      </a>
+      <a href="">
+        <img src="../assets/icons/contacts.svg" alt="" />
+        <span>Contacts</span>
+      </a>
+    </nav>
+    <div class="sidebar-footer">
+      <a class="privacy-policy-link" href="./privacy_policy.html">Privacy Policy</a>
+      <a class="legal-notice-link" href="./legal_notice.html">Legal Notice</a>
+    </div>
   `;
 
 const headerTemplate = `
