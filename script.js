92f96bfbed6938d42a628ebb069d8a86dfaf746b
Jan-Simon Boecker <jansimonboecker40@gmail.com>
2026-09-07 07:24:30 +0200

    Fix icon paths in password handling functions
    Updated icon paths for password visibility toggle.

--- script.js
+++ script.js
@@ -69,14 +69,14 @@ function initPasswordEventListener() {
 
 function handlePasswordFocus(field) {
   if (field.input.type === "password") {
-    field.icon.src = "../assets/icons/visibility_off.svg";
+    field.icon.src = "./assets/icons/visibility_off.svg";
     field.icon.alt = "hide password";
   }
 }
 
 function handleEmptyPasswordInput(field) {
   if (field.input.value === "") {
-    field.icon.src = "../assets/icons/lock.svg";
+    field.icon.src = "./assets/icons/lock.svg";
     field.icon.alt = "lock-img";
   }
 }
@@ -84,11 +84,11 @@ function handleEmptyPasswordInput(field) {
 function toggleShowPassword(field) {
   if (field.input.type === "password") {
     field.input.type = "text";
-    field.icon.src = '../assets/icons/visibility.svg';
+    field.icon.src = './assets/icons/visibility.svg';
     field.icon.alt = "show password";
   } else {
     field.input.type = "password";
-    field.icon.src = "../assets/icons/visibility_off.svg";
+    field.icon.src = "./assets/icons/visibility_off.svg";
     field.icon.alt = "hide password";
   }
 }
