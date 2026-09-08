37a828fd4ce3f657089be85588e6381490218957
Janse309 <jansimonboecker40@gmail.com>
2026-09-08 14:16:18 +0200

    update help page

--- scripts/template.js
+++ scripts/template.js
@@ -27,7 +27,7 @@ const sidebarTemplate = `
   `;
 
 const headerTemplate = `
-    <div class="content-wrapper2 header">
+    <div class="header">
       <div class="header-left">
         <button class="header-icon-button" type="button" aria-label="Header Logo">
           <img src="../assets/icons/logo-white.svg" alt="Join Logo"/>
