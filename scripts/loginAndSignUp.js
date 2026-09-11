16d2e3a63eabb2de574775204f7ce1247ed003ea
Janse309 <jansimonboecker40@gmail.com>
2026-09-11 13:03:03 +0200

    fix: update icon paths to use relative URLs

--- scripts/loginAndSignUp.js
+++ scripts/loginAndSignUp.js
@@ -42,18 +42,18 @@ function passwordInputFields() {
 
 function handlePasswordFocus(field) {
     if (field.input.type === "password") {
-        field.icon.src = "../assets/icons/visibility_off.svg";
+        field.icon.src = "./assets/icons/visibility_off.svg";
         field.icon.alt = "hide password";
     } else {
         field.input.type = "text";
-        field.icon.src = '../assets/icons/visibility.svg';
+        field.icon.src = './assets/icons/visibility.svg';
         field.icon.alt = "show password";
     }
 }
 
 function handleEmptyPasswordInput(field) {
     if (field.input.value === "") {
-        field.icon.src = "../assets/icons/lock.svg";
+        field.icon.src = "./assets/icons/lock.svg";
         field.icon.alt = "lock-img";
     }
 }
@@ -61,11 +61,11 @@ function handleEmptyPasswordInput(field) {
 function toggleShowPassword(field) {
     if (field.input.type === "password") {
         field.input.type = "text";
-        field.icon.src = '../assets/icons/visibility.svg';
+        field.icon.src = './assets/icons/visibility.svg';
         field.icon.alt = "show password";
     } else {
         field.input.type = "password";
-        field.icon.src = "../assets/icons/visibility_off.svg";
+        field.icon.src = "./assets/icons/visibility_off.svg";
         field.icon.alt = "hide password";
     }
 }
